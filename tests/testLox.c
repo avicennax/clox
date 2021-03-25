009479e93f27f8937129fa86b000797b8746580a
avicennax <simon.haxby@gmail.com>
2021-03-25 00:20:21 -0700

    Adding basic tests

--- tests/testLox.c
+++ tests/testLox.c
@@ -1,6 +1,33 @@
-
 #include "mintest.h"
+#include "chunk.h"
 
-int main(int argc, const char* argv[]) {
-    return 0;
+ int tests_run = 0;
+
+static char* test_chunk() {
+  Chunk chunk;
+  initChunk(&chunk);
+
+  int constant = addConstant(&chunk, 1.2);
+  writeChunk(&chunk, constant, 123);
+
+  mu_assert("error, chunk.capacity != 8", chunk.capacity == 8);
+  return 0;
+}
+
+static char* all_tests() {
+  mu_run_test(test_chunk);
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  char *result = all_tests();
+  if (result != 0) {
+      printf("%s\n", result);
+  }
+  else {
+      printf("ALL TESTS PASSED\n");
+  }
+  printf("Tests run: %d\n", tests_run);
+
+  return result != 0;
 }
