92721dc534554390a468a2b7f9c8ff66feef46f5
avicennax <simon.haxby@gmail.com>
2021-05-25 11:46:46 -0700

    Adding hash table stuff

--- src/lib/table.h
+++ src/lib/table.h
@@ -5,9 +5,27 @@
 #include "value.h"
 
 typedef struct {
+  ObjString* key;
+  Value value;
+} Entry;
+
+/**
+ * We use &entries[index] to do lookup where
+ * index = hash % capacity.
+ */
+typedef struct {
   int count;
   int capacity;
   Entry* entries;
 } Table;
+
+void initTable(Table* table);
+void freeTable(Table* table);
+bool tableGet(Table* table, ObjString* key, Value* value);
+bool tableSet(Table* table, ObjString* key, Value value);
+bool tableDelete(Table* table, ObjString* key);
+void tableAddAll(Table* from, Table* to);
+ObjString* tableFindString(Table* table, const char* chars,
+                           int length, uint32_t hash);
 
 #endif
